9464c5bd46629017a0aa0fe23d7dffee3a37f780
Daniel Hamidi <hamidiwebdesign@gmail.com>
2018-05-21 00:26:16 -0700

    adding background image for footer & defining color variables

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -8,21 +8,33 @@
 ?>
 
 			</div><!-- #content -->
-
-			<footer id="colophon" class="site-footer" role="contentinfo">
+			
+			<footer id="colophon" class="site-footer" role="contentinfo"> <!-- footer start -->
 
 			<div class="footer-blocks container">
 			<div class="footer-block-item"></div>
 			<div class="contact"> <h3>Contact Info</h3>
-			<p><i class="fas fa-envelope"></i><a href= href = "mailto:info@inhabitent.com">info@inhabitent.com</a></p>
-			<p><i class="fas fa-phone"></i><a href="tel:123456789">778-456-7891</a></p>
-			<p><i class="fab fa-facebook-square"></i><i class="fab fa-twitter-square"></i><i class="fab fa-google-plus-square"></i></p>
+			<p>
+			<i class="fas fa-envelope"></i><a href= href = "mailto:info@inhabitent.com">info@inhabitent.com</a>
+			</p>
+			<p>
+			<i class="fas fa-phone"></i><a href="tel:123456789">778-456-7891</a>
+			</p>
+			<p>
+			<i class="fab fa-facebook-square"></i><i class="fab fa-twitter-square"></i><i class="fab fa-google-plus-square"></i>
+			</p>
+			</div>
+			<div class="footer-block-item">
+				<h3>business hours</h3>
+			<p><span>Monday-Friday:</span> 9am to 5pm</p>
+			<p><span>Saturday:</span> 10am to 2pm</p>
+			<p><span>Sunday:</span> Closed</p>
 			</div>
 			<div class="footer-block-item"></div>
-			<div class="footer-block-item"></div>
+			<div class="text-logo"><a href="#"><img src="<?php echo get_template_directory_uri() . './assets/images/logos/inhabitent-logo-text.svg'; ?>" alt="Inhabitent logo"></a></div>
 			</div>
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					 copyright &copy 2017 inhabitent
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -11,7 +11,6 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<!-- <link rel="stylesheet" href=<?php bloginfo( 'stylesheet_url' ); ?> /> -->
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 	<?php wp_head(); ?>
