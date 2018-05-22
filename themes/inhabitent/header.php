da553f327932233b026def24015a21a99ce51e39
Daniel Hamidi <hamidiwebdesign@gmail.com>
2018-05-22 16:33:07 -0700

    creating file single.php and flex for nav bar and site branding

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -25,34 +25,30 @@
 			</a>
 
 			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<img src="<?php echo get_template_directory_uri() . './assets/images/logos/inhabitent-logo-tent-white.svg'; ?>" class="logo"
-						    alt="Inhabitent Logo" />
-					</a>
-
-					<!-- <div class="logo" >
-						<a href="#"></a>
-					</div> -->
-					<h1 class="site-title screen-reader-text">
+				<div class="container">
+					<div class="site-branding">
 						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-							<?php bloginfo( 'name' ); ?>
+							<img src="<?php echo get_template_directory_uri() . './assets/images/logos/inhabitent-logo-tent-white.svg'; ?>" class="logo"
+							    alt="Inhabitent Logo" />
 						</a>
-					</h1>
-					<p class="site-description">
-						<?php bloginfo( 'description' ); ?>
-					</p>
+					</div>
+					<nav id="site-navigation" class="main-navigation" role="navigation">
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+							<?php esc_html( 'Primary Menu' ); ?>
+						</button>
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					</nav>
 				</div>
-				<!-- .site-branding -->
-
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-						<?php esc_html( 'Primary Menu' ); ?>
-					</button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav>
-				<!-- #site-navigation -->
-			</header>
-			<!-- #masthead -->
-
-			<div id="content" class="site-content">
+				
+				<h1 class="site-title screen-reader-text">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<?php bloginfo( 'name' ); ?>
+					</a>
+				</h1>
+				<p class="site-description">
+					<?php bloginfo( 'description' ); ?>
+				</p>
+		</div>
+		<!-- #site-navigation -->
+		</header>
+		<!-- #masthead -->
