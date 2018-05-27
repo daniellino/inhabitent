ea66222d4542e9ca8db1e91db952e326701cbc8f
Daniel Hamidi <hamidiwebdesign@gmail.com>
2018-05-26 17:45:11 -0700

    styling About page

--- themes/inhabitent/page-templates/about.php
+++ themes/inhabitent/page-templates/about.php
@@ -14,33 +14,40 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-        
-	</header><!-- .entry-header -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
-	<div class="entry-content">
-		<!-- Get our custom field -->
-        
-       <h2> <?php $story_Title = CFS()->get_field_info( 'our_story' );
+				</header>
+				<!-- .entry-header -->
+
+				<div class="entry-content">
+					<!-- Get our custom field -->
+
+					<h2>
+						<?php $story_Title = CFS()->get_field_info( 'our_story' );
 
             echo $story_Title['label'];?>
-</h2>
+					</h2>
 
-<?php echo CFS()->get('our_story')?>
-<h2> <?php $props = CFS()->get_field_info( 'our_team' );
+					<?php echo CFS()->get('our_story')?>
+					<h2>
+						<?php $props = CFS()->get_field_info( 'our_team' );
 
-echo $props['label'];?>
-</h2>
-<?php echo CFS()->get('our_team')?>
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+							echo $props['label'];?>
+					</h2>
+					<?php echo CFS()->get('our_team')?>
+				</div>
+				<!-- .entry-content -->
+			</article>
+			<!-- #post-## -->
 
 			<?php endwhile; // End of the loop. ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</main>
+		<!-- #main -->
+	</div>
+	<!-- #primary -->
 
 
-<?php get_footer(); ?>
+	<?php get_footer(); ?>
