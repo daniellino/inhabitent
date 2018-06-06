59b736074d973874ca51294cc735f827f995bc70
Daniel Hamidi <hamidiwebdesign@gmail.com>
2018-06-05 17:10:13 -0700

    Styling single product page

--- themes/inhabitent/single-product.php
+++ themes/inhabitent/single-product.php
@@ -14,20 +14,27 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<header class="entry-header single-product-flex-wrapper">
+				<header class="entry-header">
+					
 					<?php if ( has_post_thumbnail() ) : ?>
 					<?php the_post_thumbnail( 'large' ); ?>
 					<?php endif; ?>
-
-					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					
+					
 
 				
 					<!-- .entry-meta -->
 				</header>
 				<!-- .entry-header -->
 
+				<div class = "product-content-wrapper">
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				<div class = "product-price-container">
+				<?php echo CFS()->get( 'price' ); ?>
+				</div>
+				<!-- end of product-price-container -->
 				<div class="entry-content">
-					<?php echo CFS()->get( 'price' ); ?>
+					
 					<?php the_content(); ?>
 					<?php
 			wp_link_pages( array(
@@ -39,13 +46,8 @@ get_header(); ?>
 				<!-- .entry-content -->
 			
 
-				<footer class="entry-footer">
-					<?php red_starter_entry_footer(); ?>
-				</footer>
-				<!-- .entry-footer -->
-			</article>
-			<!-- #post-## -->
-			<div class="social-buttons">
+				
+				<div class="social-buttons">
 					
 					<a href="#" class="sharing-button">
 						<i class="fab fa-facebook-f"></i>Like</a>
@@ -54,8 +56,13 @@ get_header(); ?>
 					<a href="#" class="sharing-button">
 						<i class="fab fa-pinterest-square"></i>Pin</a>
 				</div>
+				</div>
+				<!-- end of product-content-wrapper -->
+			</article>
+			<!-- #post-## -->
+		
 				
-			<?php the_post_navigation(); ?>
+		
 
 
 
